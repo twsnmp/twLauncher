3a564e2eb7b032e5a3a1937c0e1bff85fce3eb00
twsnmp <ymi@twise.co.jp>
2023-04-29 09:09:33 +0900

    Flowbiteとfontawsomeを追加

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,28 +3,21 @@
   import TWPCAP from "./TWPCAP.svelte";
   import TWWifiScan from "./TWWifiScan.svelte";
   import TWWinLog from "./TWWinLog.svelte";
+  import {GetInfo} from "../wailsjs/go/main/App";
+
   let info = {
     Version: "",
     Env: "",
     Ifaces: [],
     PcapVersion: "",
   };
-  window.go.main.App.GetInfo().then((result) => {
+  GetInfo().then((result) => {
     info = result;
   });
 </script>
 
 <main>
   <div class="window">
-    {#if info.Env == "darwin"}
-      <div class="title-bar">
-        <div class="title-bar-text">TWSNMP起動/設定ツール</div>
-        <div class="title-bar-controls">
-          <button aria-label="Minimize" on:click={window.runtime.WindowMinimise} />
-          <button aria-label="Close" on:click={window.runtime.Quit} />
-        </div>
-      </div>
-    {/if}
     <div class="window-body" data-wails-no-drag>
       <TWSNMP env={info.Env} />
       <TWPCAP env={info.Env} ifaces={info.Ifaces} pcapVersion={info.PcapVersion} />
@@ -40,38 +33,3 @@
     </div>
   </div>
 </main>
-
-<style>
-  div.window {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    padding-bottom: 15px;
-    box-sizing: border-box;
-    min-height: 100vh;
-  }
-
-  div.status-bar {
-    width: 99%;
-    position: absolute;
-    bottom: 0;
-  }
- :global(fieldset+fieldset) {
-    margin-top: 10px;
-  }
-  :global(label) {
-    width: 80px;
-    margin-left: 10px;
-  }
-  :global(input[type="number"]) {
-    width: 50px;
-  }
-  :global(fieldset input[type="text"]) {
-    line-height: normal;
-  }
-  :global(input[type="text"]:disabled) {
-    color: black;
-    line-height: normal;
-  }
-
-</style>
